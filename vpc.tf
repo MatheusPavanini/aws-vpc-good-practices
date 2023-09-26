30fdfe024694013f7e383ed63048f19b763e4bf3
MatheusPavanini <matheus.ppj@gmail.com>
2023-09-26 19:04:00 -0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,9 +7,11 @@ module "vpc"  {
   azs             = ["us-east-1a","us-east-1b","us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  #enable_nat_gateway = true
-  #enable_vpn_gateway = true
+  
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+  enable_dns_hostnames = true
+  single_nat_gateway = true
 
   tags = {
 			Terraform = "true"
